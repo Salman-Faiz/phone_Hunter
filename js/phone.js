ce5aa70fa18b25e18177b9e2e2ff62ff5dea72c5
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-03-15 16:03:57 +0600

    show all products  added using js

--- js/phone.js
+++ js/phone.js
@@ -6,51 +6,54 @@
 
 // async ,await function to load data
 
-const loadPhone = async (searchText) => {
-    // hard coded api url
-    // const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText, isShowAll) => {
+  // hard coded api url
+  // const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
 
-    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    const data = await response.json();
-    // console.log(data.data);
-    const phones = data.data;
-    // console.log(phone);
-    displayPhone(phones);
-   
-    
+  const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+  const data = await response.json();
+  console.log(data.data);
+  const phones = data.data;
+  // console.log(phone);
+  displayPhone(phones, isShowAll);
+
+
 }
 // loadPhone();
 
-const displayPhone = phones => {
+const displayPhone = (phones, isShowAll) => {
 
-    // console.log(phones);
-    // get the container
-    const phoneDiv = document.getElementById('phone-container');
-    // clear phone container cards before adding new cards
-    phoneDiv.textContent="";
+  // console.log(phones);
+  // get the container
+  const phoneDiv = document.getElementById('phone-container');
+  // clear phone container cards before adding new cards
+  phoneDiv.textContent = "";
 
 
- const showAllContainer = document.getElementById('showAll');
-//  console.log(phones.length);
- if(phones.length > 9){
+  const showAllContainer = document.getElementById('showAll');
+  // hide show all btn conditions
+  if (phones.length > 6 && !isShowAll) {
     showAllContainer.classList.remove('hidden');
-    
- }
- else{
+
+  }
+  else {
     showAllContainer.classList.add('hidden');
- }
+  }
+  console.log(isShowAll, "is show all")
 
 
-// showing display limit...
-   const displayLimit= phones.slice(0, 9);
+  // showing display limit 6 if showall btn not clicked.
+  if (!isShowAll) {
+    phones = phones.slice(0, 6);
+  }
 
-   displayLimit.forEach(phone => {
-        // console.log(phone);
-        // create the child element
-        const phoneDetails = document.createElement('div');
-        phoneDetails.classList = `card bg-base-100 shadow-xl`
-        // set innerHtml
-        phoneDetails.innerHTML = `<figure class="px-10 pt-10">
+  phones.forEach(phone => {
+    // console.log(phone);
+    //********** */ create the child element
+    const phoneDetails = document.createElement('div');
+    phoneDetails.classList = `card bg-base-100 shadow-xl`
+    //********** */ set innerHtml
+    phoneDetails.innerHTML = `<figure class="px-10 pt-10">
         <img src="${phone.image}" alt="" class="rounded-xl" />
       </figure>
       <div class="card-body items-center text-center">
@@ -61,36 +64,44 @@ const displayPhone = phones => {
         </div>
       </div>
         `
-        // appendChild
-        phoneDiv.appendChild(phoneDetails);
+    //********** appendChild
+    phoneDiv.appendChild(phoneDetails);
 
- 
-    });
-// to remove the loading spinner..putting it after getting all data
-    loadingSpinner(false);
+
+  });
+  // ********** to remove the loading spinner.. after getting all data
+  loadingSpinner(false);
 
 }
-// on click event handler.. @@@@@@@@
+//********** on click event handler.SEARCH. @@@@@@@@
 
-const searchPhones = () => {
+const searchPhones = (isShowAll) => {
   loadingSpinner(true);
-    const searchData = document.getElementById('Search_field');
-    const searchValue = searchData.value;
-    // console.log(searchValue);
-    loadPhone(searchValue);
-    searchData.value = '';
+  const searchData = document.getElementById('Search_field');
+  const searchValue = searchData.value;
+  // console.log(searchValue);
+  loadPhone(searchValue, isShowAll);
+  // searchData.value = '';
 }
-// loading-spinner added
-const loadingSpinner = (isLoading) =>{
+//********** loading-spinner added
+const loadingSpinner = (isLoading) => {
   const loading = document.getElementById('loading-spinner');
 
-  if(isLoading === true){
-     loading.classList.remove('hidden');
+  if (isLoading === true) {
+    loading.classList.remove('hidden');
   }
-  else{
+  else {
     loading.classList.add('hidden');
   }
- 
+
+}
+
+// show all button !!!!!NOT RECOMMENDED
+const showAllElements = () => {
+  // console.log('clickedd')
+  searchPhones(true);
+
+
 }
 
 
